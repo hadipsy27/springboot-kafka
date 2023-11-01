71d832bb28e4a3af930878c05067469511d1a11d
hadipsy27 <hadipsy27@gmail.com>
2023-11-01 09:39:19 +0700

    First commit

---
+++ src/main/java/com/labs/kafka/SpringbootKafkaLearnApplication.java
@@ -0,0 +1,13 @@
+package com.labs.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labs/kafka/SpringbootKafkaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
