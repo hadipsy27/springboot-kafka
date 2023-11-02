04b1fb8ea4fc054a808820f8725a54ab241178e1
hadipsy27 <hadipsy27@gmail.com>
2023-11-02 10:51:50 +0700

    Kafka - 209. Create Kafka Producer to Produce JSON Message

---
+++ src/main/java/com/labs/kafka/kafka/JsonKafkaProducer.java
@@ -0,0 +1,30 @@
+package com.labs.kafka.kafka;
+
+import com.labs.kafka.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+
+    @Autowired
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    public void sendMessage(User data) {
+        LOGGER.info(String.format("Sending message: " + data.toString()));
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "javaguides")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
